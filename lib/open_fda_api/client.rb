7cebbd8172cb7d0bb6f86ec3d47869adaed04913
Hebron George <hebrontgeorge@gmail.com>
2022-01-18 23:18:57 -0500

    Expose a drugs class to interact with endpoints in the drugs api

--- lib/open_fda_api/client.rb
+++ lib/open_fda_api/client.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
+require_relative "./drugs"
+
 module OpenFdaApi
   # Gives you access to the main nouns in the openFDA API
   class Client
     attr_reader :api_key
 
-    def initialize(api_key:)
+    def initialize(api_key: nil)
       @api_key = api_key
+    end
+
+    def drugs
+      OpenFdaApi::Drugs.new
     end
   end
 end
