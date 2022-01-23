a292dbd755fc506673016bfb5a9e7899c5c56f0f
Hebron George <hebrontgeorge@gmail.com>
2022-01-23 11:05:17 -0500

    Use faraday for http requests

--- lib/open_fda_api/client.rb
+++ lib/open_fda_api/client.rb
@@ -1,18 +1,35 @@
 # frozen_string_literal: true
 
 require_relative "./drugs"
+require 'faraday'
+require 'faraday_middleware'
 
 module OpenFdaApi
   # Gives you access to the main nouns in the openFDA API
   class Client
-    attr_reader :api_key
+    attr_reader :api_key, :adapter
 
-    def initialize(api_key: nil)
+    BASE_URL = "https://api.fda.gov"
+
+    def initialize(api_key: nil, adapter: Faraday.default_adapter, stubs: nil)
       @api_key = api_key
+      @adapter = adapter
+      @stubs   = stubs
     end
 
     def drugs
-      OpenFdaApi::Drugs.new
+      OpenFdaApi::Drugs.new(self)
+    end
+
+    def connection
+      @connection ||= Faraday.new(BASE_URL) do |conn|
+        conn.request :json
+
+        conn.response :dates
+        conn.response :json, content_type: "application/json"
+
+        conn.adapter adapter, @stubs
+      end
     end
   end
 end
