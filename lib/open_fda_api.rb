6a2f05cc67cbc79bb464438d50afda7d49ddb833
Hebron George <hebrontgeorge@gmail.com>
2022-01-24 01:35:00 -0500

    Delete the client method; autoload classes

--- lib/open_fda_api.rb
+++ lib/open_fda_api.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 require_relative "open_fda_api/version"
-require_relative "open_fda_api/client"
 
 # A Ruby wrapper for the openFDA API: https://open.fda.gov/apis/
 module OpenFdaApi
   class Error < StandardError; end
 
-  # TODO: Refactor client method out, it's literally just forwarding everything...
-  def self.client(api_key: nil, adapter: Faraday.default_adapter, stubs: nil)
-    OpenFdaApi::Client.new(api_key: api_key, adapter: adapter, stubs: stubs)
-  end
+  autoload :Client, "open_fda_api/client"
+  autoload :Drugs,  "open_fda_api/drugs"
+  autoload :QueryInputs, "open_fda_api/query_inputs"
+  autoload :QueryBuilder, "open_fda_api/query_builder"
 end
