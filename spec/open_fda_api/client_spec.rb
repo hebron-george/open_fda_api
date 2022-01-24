066885300e39be257e19a75479ad64401e516e12
Hebron George <hebrontgeorge@gmail.com>
2022-01-24 06:28:41 -0500

    Client tests for device and food endpoints

--- spec/open_fda_api/client_spec.rb
+++ spec/open_fda_api/client_spec.rb
@@ -8,4 +8,18 @@ RSpec.describe "OpenFdaApi::Client" do
       expect(drugs).to be_a(OpenFdaApi::Drugs)
     end
   end
+
+  context "#device" do
+    subject(:device) { instance.device }
+    it "has the right type" do
+      expect(device).to be_a(OpenFdaApi::Device)
+    end
+  end
+
+  context "#food" do
+    subject(:food) { instance.food }
+    it "has the right type" do
+      expect(food).to be_a(OpenFdaApi::Food)
+    end
+  end
 end
